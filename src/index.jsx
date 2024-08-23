587c294593ae03b331ca78b6b5e2024fb5ebcd7b
manojkumar <116948057+Vmkmanoj@users.noreply.github.com>
2024-08-23 12:19:29 +0530

    adding selecting news button

--- src/index.jsx
+++ src/index.jsx
@@ -8,21 +8,20 @@ function Mortal(){
 
     const [pageload,setPageload]  = useState([]);
 
+    const [topics,setTopic] = useState("business");
+
 
     const api = async () =>{
 
 
         let apikey = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=79052ea9c63248179ef0f6775ae5c728";
-
         let fec = await fetch(apikey);
-
         let json = await fec.json()
-
         setTimeout(()=>{
-
             setNews(json.articles);
-
         },2000)
+
+      
 
        
 
@@ -30,21 +29,34 @@ function Mortal(){
 
         console.log(pageload)
 
+        console.log(topics)
+
+    }
+
+    function handlclick(url){
+        setPageload(url)
+        window.open(url,"_blank")
+
     }
 
 
     useEffect(()=>{
-        api()
-    },[pageload])
+        api(pageload)
+    },[topics])
 
 
 
 
     return (
         <div className="container">
+              <select className="select" value={topics} onChange={(e)=>setTopic(e.target.value)}>
+                    <option value="business headlines">business headlines</option>
+                    <option  value="TechCrunch" >TechCrunch</option>
+                </select>
           {news.map((article, index) => (
             <div key={index} className="news-item">
-              <img src={article.urlToImage} onClick={()=>setPageload(article.url)} alt={article.title} className="news-image" />
+              
+              <img src={article.urlToImage} onClick={()=>handlclick(article.url)} alt={article.title} className="news-image" />
               <h2 className="news-title">{article.title}</h2>
             </div>
           ))}
